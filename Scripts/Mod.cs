2bb22e5eb390a8300fe2504d07e8e7de32377c88
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-03 23:15:04 +0200

    chore: add a basic SessionComponent

--- Scripts/Mod.cs
+++ Scripts/Mod.cs
@@ -1,5 +1,181 @@
-﻿namespace AutoMcD.SmartRotors {
-    public class Mod {
-        
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sandbox.ModAPI;
+using Sisk.Utils.Logging;
+using Sisk.Utils.Logging.DefaultHandler;
+using Sisk.Utils.Net;
+using Sisk.Utils.Profiler;
+using VRage;
+using VRage.Game.Components;
+
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
+// ReSharper disable UnreachableCode
+#pragma warning disable 162
+
+namespace AutoMcD.SmartRotors {
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Mod : MySessionComponentBase {
+        public const string NAME = "SmartRotors";
+
+        // important: set profile to false before publishing this mod.
+        public const bool PROFILE = true;
+
+        // important: change to info | warning | error or none before publishing this mod.
+        private const LogEventLevel DEFAULT_LOG_EVENT_LEVEL = LogEventLevel.All;
+
+        private const string LOG_FILE_TEMPLATE = "{0}.log";
+        private const ushort NETWORK_ID = 51502;
+        private const string PROFILER_LOG_FILE = "profiler.log";
+        private const string PROFILER_SUMMARY_FILE = "profiler_summary.txt";
+        private static readonly string LogFile = string.Format(LOG_FILE_TEMPLATE, NAME);
+        private ILogger _profilerLog;
+
+        public Mod() {
+            Static = this;
+            InitializeLogging();
+        }
+
+        /// <summary>
+        ///     Logger used for logging.
+        /// </summary>
+        public ILogger Log { get; private set; }
+
+        /// <summary>
+        ///     Network to handle syncing.
+        /// </summary>
+        public Network Network { get; private set; }
+
+        /// <summary>
+        ///     The static instance.
+        /// </summary>
+        public static Mod Static { get; private set; }
+
+        private static string LogFormatter(LogEventLevel level, string message, DateTime timestamp, Type scope, string method) {
+            return $"[{timestamp:HH:mm:ss:fff}] [{new string(level.ToString().Take(1).ToArray())}] [{scope}->{method}()]: {message}";
+        }
+
+        private static void WriteProfileResults() {
+            if (Profiler.Results.Any()) {
+                using (var writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(PROFILER_SUMMARY_FILE, typeof(Mod))) {
+                    foreach (var result in Profiler.Results.OrderByDescending(x => x.Total)) {
+                        writer.WriteLine(result);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Load mod settings and create localizations.
+        /// </summary>
+        public override void LoadData() {
+            using (PROFILE ? Profiler.Measure(nameof(Mod), nameof(LoadData)) : null) {
+                LoadTranslation();
+
+                if (MyAPIGateway.Multiplayer.MultiplayerActive) {
+                    InitializeNetwork();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Save mod settings and fire OnSave event.
+        /// </summary>
+        public override void SaveData() {
+            using (PROFILE ? Profiler.Measure(nameof(Mod), nameof(SaveData)) : null) {
+                using (Log.BeginMethod(nameof(SaveData))) {
+                    Log.Flush();
+                    _profilerLog.Flush();
+                    WriteProfileResults();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Unloads all data.
+        /// </summary>
+        protected override void UnloadData() {
+            Log?.EnterMethod(nameof(UnloadData));
+            if (PROFILE) {
+                Log?.Info("Writing profiler data");
+                WriteProfileResults();
+                if (_profilerLog != null) {
+                    Log?.Info("Profiler logging stopped");
+                    _profilerLog.Flush();
+                    _profilerLog.Close();
+                    _profilerLog = null;
+                }
+            }
+
+            if (Log != null) {
+                Log.Info("Logging stopped");
+                Log.Flush();
+                Log.Close();
+                Log = null;
+            }
+
+            if (Network != null) {
+                Log?.Info("Cap network connections");
+                Network.Close();
+                Network = null;
+            }
+
+            Static = null;
+        }
+
+        /// <summary>
+        ///     Initialize the logging system.
+        /// </summary>
+        private void InitializeLogging() {
+            using (PROFILE ? Profiler.Measure(nameof(Mod), nameof(InitializeLogging)) : null) {
+                Log = Logger.ForScope<Mod>();
+                Log.Register(new LocalStorageHandler(LogFile, LogFormatter, DEFAULT_LOG_EVENT_LEVEL, PROFILE ? -1 : 500));
+
+                if (PROFILE) {
+                    _profilerLog = Logger.ForScope<Mod>();
+                    _profilerLog.Register(new LocalStorageHandler(PROFILER_LOG_FILE, (level, message, timestamp, scope, method) => message, LogEventLevel.All, 0));
+                    Profiler.SetLogger(_profilerLog.Info);
+                }
+
+                using (Log.BeginMethod(nameof(InitializeLogging))) {
+                    Log.Info("Logging initialized");
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Initialize the network system.
+        /// </summary>
+        private void InitializeNetwork() {
+            using (PROFILE ? Profiler.Measure(nameof(Mod), nameof(InitializeNetwork)) : null) {
+                using (Log.BeginMethod(nameof(InitializeNetwork))) {
+                    Log.Info("Initialize Network");
+                    Network = new Network(NETWORK_ID);
+                    Log.Info($"IsClient {Network.IsClient}, IsServer: {Network.IsServer}, IsDedicated: {Network.IsDedicated}");
+                    Log.Info("Network initialized");
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Load translations for this mod.
+        /// </summary>
+        private void LoadTranslation() {
+            using (PROFILE ? Profiler.Measure(nameof(Mod), nameof(LoadTranslation)) : null) {
+                using (Log.BeginMethod(nameof(LoadTranslation))) {
+                    var currentLanguage = MyAPIGateway.Session.Config.Language;
+                    var supportedLanguages = new HashSet<MyLanguagesEnum>();
+
+                    MyTexts.LoadSupportedLanguages($"{ModContext.ModPathData}\\Localization", supportedLanguages);
+                    if (supportedLanguages.Contains(currentLanguage)) {
+                        MyTexts.LoadTexts($"{ModContext.ModPathData}\\Localization", MyTexts.Languages[currentLanguage].CultureName);
+                        Log.Info($"Loaded {MyTexts.Languages[currentLanguage].FullCultureName} translations.");
+                    } else if (supportedLanguages.Contains(MyLanguagesEnum.English)) {
+                        MyTexts.LoadTexts($"{ModContext.ModPathData}\\Localization", MyTexts.Languages[MyLanguagesEnum.English].CultureName);
+                        Log.Warning($"No {MyTexts.Languages[currentLanguage].FullCultureName} translations found. Fall back to {MyTexts.Languages[MyLanguagesEnum.English].FullCultureName} translations.");
+                    }
+                }
+            }
+        }
     }
 }
