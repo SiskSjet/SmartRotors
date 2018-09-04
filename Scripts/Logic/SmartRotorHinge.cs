afaee3d04b7d25096aa0264cc08adec18ce8d9b8
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-04 21:21:27 +0200

    feat: automatic placement of hinge block when the base is being built

---
+++ Scripts/Logic/SmartRotorHinge.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMcD.SmartRotors.Extensions;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using Sisk.Utils.Logging;
+using Sisk.Utils.Profiler;
+using VRage.Game.Components;
+using VRage.ObjectBuilders;
+
+// ReSharper disable InlineOutVariableDeclaration
+
+namespace AutoMcD.SmartRotors.Logic {
+    public abstract class SmartRotorHinge : MyGameLogicComponent {
+        private const string ADD_HEAD_ACTION_ID = "Add Top Part";
+        private readonly string _debugName;
+        private bool _isInitialized;
+
+        protected SmartRotorHinge(string debugName) {
+            _debugName = debugName;
+            Log = Mod.Static.Log.ForScope<SmartRotorHinge>();
+        }
+
+        public override string ComponentTypeDebugString => $"{_debugName} - Game Logic";
+
+        public bool IsJustPlaced { get; private set; }
+        private ILogger Log { get; }
+        public IMyMotorAdvancedStator Stator { get; private set; }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder) {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(SmartRotorHinge), nameof(Init)) : null) {
+                using (Log.BeginMethod(nameof(Init))) {
+                    base.Init(objectBuilder);
+
+                    Stator = Entity as IMyMotorAdvancedStator;
+                    if (Stator == null) {
+                        return;
+                    }
+
+                    IsJustPlaced = Stator.CubeGrid?.Physics != null;
+                }
+            }
+        }
+
+        public override void OnAddedToScene() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(SmartRotorHinge), nameof(OnAddedToScene)) : null) {
+                using (Log.BeginMethod(nameof(OnAddedToScene))) {
+                    if (_isInitialized) {
+                        return;
+                    }
+
+                    if (Stator.IsProjected()) {
+                        return;
+                    }
+
+                    if (Stator.Top == null) {
+                        var instantBuild = MyAPIGateway.Session.CreativeMode || MyAPIGateway.Session.HasCreativeRights && MyAPIGateway.Session.EnableCopyPaste;
+
+                        List<IMyTerminalAction> defaultActions;
+                        MyAPIGateway.TerminalControls.GetActions<IMyMotorAdvancedStator>(out defaultActions);
+
+                        var attach = defaultActions.FirstOrDefault(x => x.Id == ADD_HEAD_ACTION_ID)?.Action;
+                        if (attach == null) {
+                            return;
+                        }
+
+                        attach(Stator);
+
+                        if (instantBuild && Stator.Top != null) {
+                            var slimBlock = Stator.Top.SlimBlock;
+                            var welderMountAmount = slimBlock.MaxIntegrity - slimBlock.Integrity;
+                            slimBlock.IncreaseMountLevel(welderMountAmount, Stator.OwnerId);
+
+                            Log.Debug("Hinge Head placed.");
+                        }
+                    }
+
+                    _isInitialized = true;
+                }
+            }
+        }
+    }
+}
--- Scripts/Logic/SmartSolarRotorBase.cs
+++ Scripts/Logic/SmartSolarRotorBase.cs
@@ -1,5 +1,12 @@
-﻿using Sandbox.Common.ObjectBuilders;
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
 using Sisk.Utils.Logging;
+using Sisk.Utils.Profiler;
+using VRage;
+using VRage.Game;
 using VRage.Game.Components;
 
 namespace AutoMcD.SmartRotors.Logic {
@@ -13,6 +20,65 @@ namespace AutoMcD.SmartRotors.Logic {
 
         private ILogger Log { get; }
 
-        protected override void PlaceSmartRotorHinge() { }
+        protected override void PlaceSmartRotorHinge() {
+            using (Mod.PROFILE ? Profiler.Measure(nameof(SmartSolarRotorBase), nameof(PlaceSmartRotorHinge)) : null) {
+                using (Log.BeginMethod(nameof(PlaceSmartRotorHinge))) {
+                    Log.Debug("Try to place smart solar hinge.");
+                    var head = Stator.Top;
+                    if (head == null) {
+                        return;
+                    }
+
+                    var cubeGrid = head.CubeGrid;
+                    var gridSize = cubeGrid.GridSize;
+                    var forward = head.WorldMatrix.Forward;
+                    var up = head.WorldMatrix.Up;
+
+                    var headPosition = head.GetPosition();
+                    var origin = headPosition + up * gridSize;
+                    var hingePosition = cubeGrid.WorldToGridInteger(origin);
+                    if (cubeGrid.CubeExists(hingePosition)) {
+                        Log.Debug($"There is already a block on this position: {hingePosition}.");
+                        return;
+                    }
+
+                    var canPlaceCube = cubeGrid.CanAddCube(hingePosition);
+                    if (!canPlaceCube) {
+                        Log.Debug($"Unable to place block on this position: {hingePosition}.");
+                    }
+
+                    try {
+                        var instantBuild = MyAPIGateway.Session.CreativeMode || MyAPIGateway.Session.HasCreativeRights && MyAPIGateway.Session.EnableCopyPaste;
+                        var buildPercent = instantBuild ? 1 : 0.00001525902f;
+                        var hingeBuilder = new MyObjectBuilder_MotorAdvancedStator {
+                            SubtypeName = "MA_SmartRotor_Solar_Hinge",
+                            Owner = Stator.OwnerId,
+                            BuiltBy = Stator.OwnerId,
+                            BuildPercent = buildPercent,
+                            IntegrityPercent = buildPercent
+                        };
+
+                        var cubeGridBuilder = new MyObjectBuilder_CubeGrid {
+                            CreatePhysics = true,
+                            GridSizeEnum = head.CubeGrid.GridSizeEnum,
+                            PositionAndOrientation = new MyPositionAndOrientation(origin, up, forward)
+                        };
+
+                        cubeGridBuilder.CubeBlocks.Add(hingeBuilder);
+
+                        var gridsToMerge = new List<MyObjectBuilder_CubeGrid> { cubeGridBuilder };
+                        (cubeGrid as MyCubeGrid)?.PasteBlocksToGrid(gridsToMerge, 0, false, false);
+
+                        //MyTransparentGeometry.AddLineBillboard(MyStringId.GetOrCompute("Square"), new Color(Color.WhiteSmoke, 0.8f).ToVector4(), headPosition + up * 1.5, up, 2.5f, .05f);
+                        //MyTransparentGeometry.AddPointBillboard(MyStringId.GetOrCompute("WhiteDot"), new Color(Color.CornflowerBlue, 0.8f).ToVector4(), headPosition + up * 1.5, .5f, 0);
+                        //MyTransparentGeometry.AddBillboardOriented(MyStringId.GetOrCompute("Square"), new Color(Color.CornflowerBlue, 0.8f).ToVector4(), origin, left, up, 1f);
+
+                        Log.Debug("Hinge placed");
+                    } catch (Exception exception) {
+                        Log.Error(exception);
+                    }
+                }
+            }
+        }
     }
 }
