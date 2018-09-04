2fc5b3bd51366da80d77505780c774262eb9382d
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-04 14:58:55 +0200

    chore: add a basic `SmartSolarRotor` logic implementation

---
+++ Scripts/Logic/SmartSolarRotorBase.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sisk.Utils.Logging;
+using VRage.Game.Components;
+
+namespace AutoMcD.SmartRotors.Logic {
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_MotorAdvancedStator), false, LB_SMART_SOLAR_ROTOR)]
+    public class SmartSolarRotorBase : SmartRotorBase {
+        private const string LB_SMART_SOLAR_ROTOR = "MA_SmartRotor_Solar_Base";
+
+        public SmartSolarRotorBase() : base(nameof(SmartSolarRotorBase)) {
+            Log = Mod.Static.Log.ForScope<SmartSolarRotorBase>();
+        }
+
+        private ILogger Log { get; }
+
+        protected override void PlaceSmartRotorHinge() { }
+    }
+}
